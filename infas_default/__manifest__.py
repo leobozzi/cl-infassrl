016069df9047f17c68990e383f431e87f5bea87e
leobozzi <lbozzi@gmail.com>
2022-03-18 10:45:36 -0300

    Initial commit

--- infas_default/__manifest__.py
+++ infas_default/__manifest__.py
@@ -169,7 +169,7 @@
 
         # Comma-separated list of server-wide modules, there are modules loaded
         # automatically even if you do not create any database.
-            'server_wide_modules = base,web',
+            'server_wide_modules = base',
 
         # Filter listed database REGEXP
             'dbfilter =',
