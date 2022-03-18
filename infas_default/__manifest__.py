922d8b6c0ed7e7fe0e2d44a55888aa7219856803
leobozzi <lbozzi@gmail.com>
2022-03-18 16:38:13 -0300

    Initial commit

--- infas_default/__manifest__.py
+++ infas_default/__manifest__.py
@@ -301,7 +301,7 @@
         # ADHOC
         # ==========================================================================================
         # 'https://github.com/ingadhoc/account-analytic ingadhoc-account-analytic',
-        # 'https://github.com/ingadhoc/account-financial-tools ingadhoc-account-financial-tools',  # noqa
+        'https://github.com/ingadhoc/account-financial-tools ingadhoc-account-financial-tools',  # noqa
         'https://github.com/ingadhoc/account-payment ingadhoc-account-payment',
         # 'https://github.com/ingadhoc/aeroo_reports ingadhoc-aeroo_reports',
         # 'https://github.com/ingadhoc/argentina-reporting ingadhoc-argentina-reporting',
