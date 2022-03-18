db0f20346aa70d9d2df9eaadd12b929c094434cc
leobozzi <lbozzi@gmail.com>
2022-03-18 11:28:09 -0300

    Initial commit

--- infas_default/__manifest__.py
+++ infas_default/__manifest__.py
@@ -295,7 +295,7 @@
         # 'https://github.com/OCA/timesheet oca-timesheet',
         # 'https://github.com/OCA/vertical-association oca-vertical-association',
         # 'https://github.com/OCA/website oca-website',
-        # 'https://github.com/OCA/web oca-web',
+        'https://github.com/OCA/web.git',
         # 'https://github.com/OCA/account-analytic',
 
         # ADHOC
