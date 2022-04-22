806433dcfeb9e284a2efeefa86543eedf25efed5
leobozzi <lbozzi@gmail.com>
2022-04-22 15:30:15 -0300

    Initial commit

--- infas_default/__manifest__.py
+++ infas_default/__manifest__.py
@@ -19,7 +19,7 @@
 # -----------------------------------------------------------------------------------
 {
     'name': 'infassrl',
-    'version': '14.0.1.0.0',
+    'version': '15.0.1.0.0',
     'license': 'Other OSI approved licence',
     'category': 'Tools',
     'summary': 'Customización Infas SRL',
